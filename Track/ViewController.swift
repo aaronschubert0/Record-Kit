325d0d6fd5034419db1a42ead362ac1aa1e77e9f
Aaron Schubert <aaronschubert0@googlemail.com>
2016-01-08 21:30:57 +0000

    Initial Commit

---
+++ Track/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Track
+//
+//  Created by Aaron Schubert on 08/01/2016.
+//  Copyright © 2016 aaronschubert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
